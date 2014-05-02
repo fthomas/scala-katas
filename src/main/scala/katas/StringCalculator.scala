f315903448dc3ed750384bce594462eda17f7f65
Frank S. Thomas <frank@timepit.eu>
2014-05-02 17:55:34 +0200

    4. Refactor

--- src/main/scala/katas/StringCalculator.scala
+++ src/main/scala/katas/StringCalculator.scala
@@ -2,13 +2,12 @@ package katas
 
 object StringCalculator {
   def add(numbers: String): Int = {
-    if (numbers.isEmpty) 0
-    else {
-      val lines = numbers.lines.toList
-      val (numberLines, delimiter) =
-        if (lines(0).startsWith("//")) (lines.drop(1), lines(0).stripPrefix("//"))
-        else (lines, ",")
-      numberLines.flatMap(_.split(delimiter)).map(_.toInt).sum
-    }
+    val marker = "//"
+    val lines = numbers.lines
+    val delimiter =
+      if (numbers.startsWith(marker)) lines.next().stripPrefix(marker)
+      else ","
+
+    lines.flatMap(_.split(delimiter)).map(_.toInt).sum
   }
 }
