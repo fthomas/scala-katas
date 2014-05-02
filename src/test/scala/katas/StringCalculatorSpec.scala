30e8858cde1fb1174b491faa4bc7a47cd38a179b
Frank S. Thomas <frank@timepit.eu>
2014-05-02 17:34:12 +0200

    2. Our add already allows more than two numbers

--- src/test/scala/katas/StringCalculatorSpec.scala
+++ src/test/scala/katas/StringCalculatorSpec.scala
@@ -14,4 +14,8 @@ class StringCalculatorSpec extends FlatSpec with Matchers {
   it should "return 3 for the argument \"1,2\"" in {
     add("1,2") should be(3)
   }
+  it should "handle more than two numbers" in {
+    add("1,2,3") should be(6)
+    add("4,5,6,7,0") should be(22)
+  }
 }
