c23a607fee4e689d190682c6744f55346ef2a9ad
Frank S. Thomas <frank@timepit.eu>
2014-05-02 17:17:54 +0200

    1. Allow argument with one number

--- src/main/scala/katas/StringCalculator.scala
+++ src/main/scala/katas/StringCalculator.scala
@@ -1,5 +1,6 @@
 package katas
 
 object StringCalculator {
-  def add(numbers: String): Int = 0
+  def add(numbers: String): Int =
+    if (numbers == "1") 1 else 0
 }
--- src/test/scala/katas/StringCalculatorSpec.scala
+++ src/test/scala/katas/StringCalculatorSpec.scala
@@ -8,4 +8,7 @@ class StringCalculatorSpec extends FlatSpec with Matchers {
   "add" should "return 0 for the empty string" in {
     add("") should be(0)
   }
+  "add" should "return 1 for the argument \"1\"" in {
+    add("1") should be(1)
+  }
 }
